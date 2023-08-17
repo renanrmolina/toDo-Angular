3c3bae3ed1c8adc3e7b0c516ff3ad2d371c16b23
Renan Molina <renanrobertomolina@hotmail.com>
2023-08-16 22:08:52 -0300

    Aula-16-08

---
+++ todo/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+
+export class TodoListComponent{
+  taskArray = [{ taskName: 'Adicionar tarefas', isCompleted: false }];
+  onSubmit(form: NgForm){
+    console.log(form.controls);
+   
+    this.taskArray.push({
+    taskName: form.controls['task'].value,
+    isCompleted: false
+    });
+    form.reset();
+  }
+  onDelete(index: number){
+    this.taskArray.splice(index, 1);
+    }
+  onCheck(index: number){
+      this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+      }
+}
